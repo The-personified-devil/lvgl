fc7551705a162059fe6e032456d8b3a9a65cc818
The personified devil <leonhard60-git@yahoo.com>
2021-02-21 23:11:56 +0100

    Modified the by the makfiles returned file paths

--- src/lv_core/lv_core.mk
+++ src/lv_core/lv_core.mk
@@ -1,11 +1,11 @@
-CSRCS += lv_group.c
-CSRCS += lv_indev.c
-CSRCS += lv_disp.c
-CSRCS += lv_obj.c
-CSRCS += lv_refr.c
-CSRCS += lv_style.c
+CSRCS += $(LVGL_DIR_NAME)/src/lv_core/lv_group.c
+CSRCS += $(LVGL_DIR_NAME)/src/lv_core/lv_indev.c
+CSRCS += $(LVGL_DIR_NAME)/src/lv_core/lv_disp.c
+CSRCS += $(LVGL_DIR_NAME)/src/lv_core/lv_obj.c
+CSRCS += $(LVGL_DIR_NAME)/src/lv_core/lv_refr.c
+CSRCS += $(LVGL_DIR_NAME)/src/lv_core/lv_style.c
 
 DEPPATH += --dep-path $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/lv_core
-VPATH += :$(LVGL_DIR)/$(LVGL_DIR_NAME)/src/lv_core
+VPATH += $(LVGL_DIR)
 
 CFLAGS += "-I$(LVGL_DIR)/$(LVGL_DIR_NAME)/src/lv_core"
--- src/lv_draw/lv_draw.mk
+++ src/lv_draw/lv_draw.mk
@@ -1,16 +1,15 @@
-CSRCS += lv_draw_mask.c
-CSRCS += lv_draw_blend.c
-CSRCS += lv_draw_rect.c
-CSRCS += lv_draw_label.c
-CSRCS += lv_draw_line.c
-CSRCS += lv_draw_img.c
-CSRCS += lv_draw_arc.c
-CSRCS += lv_draw_triangle.c
-CSRCS += lv_img_decoder.c
-CSRCS += lv_img_cache.c
-CSRCS += lv_img_buf.c
+CSRCS += $(LVGL_DIR_NAME)/src/lv_draw/lv_draw_mask.c
+CSRCS += $(LVGL_DIR_NAME)/src/lv_draw/lv_draw_blend.c
+CSRCS += $(LVGL_DIR_NAME)/src/lv_draw/lv_draw_rect.c
+CSRCS += $(LVGL_DIR_NAME)/src/lv_draw/lv_draw_label.c
+CSRCS += $(LVGL_DIR_NAME)/src/lv_draw/lv_draw_line.c
+CSRCS += $(LVGL_DIR_NAME)/src/lv_draw/lv_draw_img.c
+CSRCS += $(LVGL_DIR_NAME)/src/lv_draw/lv_draw_arc.c
+CSRCS += $(LVGL_DIR_NAME)/src/lv_draw/lv_draw_triangle.c
+CSRCS += $(LVGL_DIR_NAME)/src/lv_draw/lv_img_decoder.c
+CSRCS += $(LVGL_DIR_NAME)/src/lv_draw/lv_img_cache.c
+CSRCS += $(LVGL_DIR_NAME)/src/lv_draw/lv_img_buf.c
 
 DEPPATH += --dep-path $(LVGL_DIR)/$(LVGL_DIR_NAME)/src/lv_draw
-VPATH += :$(LVGL_DIR)/$(LVGL_DIR_NAME)/src/lv_draw
 
 CFLAGS += "-I$(LVGL_DIR)/$(LVGL_DIR_NAME)/src/lv_draw"
